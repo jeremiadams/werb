47daa75fc7a93840f99d652f3466a7f81e782366
Jeremi Adams <jeremiadamss@gmail.com>
2023-01-01 17:11:28 +0100

    heroku commit

--- server/index.js
+++ server/index.js
@@ -89,9 +89,9 @@ app.get('/coins', (req,res) => {
     
 })
 
-app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
-  });
+// app.get('*', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+//   });
 
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
